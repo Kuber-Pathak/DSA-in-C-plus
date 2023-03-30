3ea315df0b1b2298d9af94bebcc31375ac7fbb63
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-03-30 08:06:09 +0545

    modified

---
+++ Binary Tree/porstOrder_traversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void postOrder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    //     1
+    //    / \
+    //   2   3
+    //  / \
+    // 4   5
+    postOrder(root);
+    return 0;
+}
