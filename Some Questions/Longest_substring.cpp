3e9a9a5e42e674e4d487550838921e1d82715f5f
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-03-30 07:46:22 +0545

    modified

--- Some Questions/Longest_substring.cpp
+++ Some Questions/Longest_substring.cpp
@@ -8,9 +8,11 @@ int main()
     int maxLen = 0, start = -1;
     for (int i = 0; i < s.size(); i++)
     {
-        if (dict[s[i]] > start)
 
+        if (dict[s[i]] > start)
+        {
             start = dict[s[i]];
+        }
         dict[s[i]] = i;
         maxLen = max(maxLen, i - start);
     }
